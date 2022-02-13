3b4ac2907faa32a3daba765f94ac8a4191e402f3
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2022-02-13 17:28:58 +0530

    added mobilenet

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -4,12 +4,13 @@ import torchvision.models as models
 from exceptions.exceptions import InvalidBackboneError
 
 
-class ResNetSimCLR(nn.Module):
+class NetSimCLR(nn.Module):
 
     def __init__(self, base_model, out_dim):
         super(ResNetSimCLR, self).__init__()
-        self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
-                            "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)}
+        self.net_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
+                          "resnet50": models.resnet50(pretrained=False, num_classes=out_dim),
+                          "mobilenetv2": models..mobilenet_v2(pretrained=False, num_classes=out_dim)}
 
         self.backbone = self._get_basemodel(base_model)
         dim_mlp = self.backbone.fc.in_features
@@ -19,10 +20,10 @@ class ResNetSimCLR(nn.Module):
 
     def _get_basemodel(self, model_name):
         try:
-            model = self.resnet_dict[model_name]
+            model = self.net_dict[model_name]
         except KeyError:
             raise InvalidBackboneError(
-                "Invalid backbone architecture. Check the config file and pass one of: resnet18 or resnet50")
+                "Invalid backbone architecture. Check the config file and pass one of: resnet18 or resnet50 or mobilenetv2")
         else:
             return model
 
